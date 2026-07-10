8b91e0491436c5e5623d2897a1aa20da78444005
ZuhRa HashiMi <zuhra.tactaz@yahoo.com>
2026-07-10 12:35:16 +0430

    Number Guessing Game — Step 2: Check the guess

--- GuessGame.py
+++ GuessGame.py
@@ -1,3 +1,10 @@
 secret_number = 7
 guess = int(input("Guess a number: "))
-print(f"you guessed {guess}")
+print(f"You guessed {guess}")
+
+if guess == secret_number:
+    print("Correct!")
+elif guess > secret_number:
+    print("Too Hight!")
+else:
+    print("Too Low!")
