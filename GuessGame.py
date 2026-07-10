105fec7095d665741626bc41343bab952bb8cc14
ZuhRa HashiMi <zuhra.tactaz@yahoo.com>
2026-07-10 12:46:41 +0430

    Number Guessing Game — Step 6: Validate the guess

--- GuessGame.py
+++ GuessGame.py
@@ -9,6 +9,11 @@ print("I am thinking of a number between 1 and 10.")
 while True:
     guess = int(input("Guess a number: "))
     attempts += 1
+    
+    if guess < 1 or guess > 10:
+        print("please chose number between 1 and 10")
+        continue
+    attempts += 1
 
     if guess == secret_number:
         print(f"Correct! You guessed it in {attempts} attempts.")
