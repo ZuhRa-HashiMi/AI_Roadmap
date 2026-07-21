5ae65954400c06ae89d0a35f45333fb099a39e86
ZuhRa HashiMi <zuhra.tactaz@yahoo.com>
2026-07-21 15:25:37 +0430

    work on vector

--- Day4/Vector.py
+++ Day4/Vector.py
@@ -8,7 +8,19 @@ class Vector:
     def __str__(self):
         return str(self.values)
 
+    def __repr__(self):
+        return f"Vector({self.values})"
+
+    def __eq__(self, other):
+        return self.values == other.values
+
+    def check_dimension(self, other):
+        if len(self.values) != len(other.values):
+            raise ValueError("Vectors must have the same dimension")
+
     def __add__(self, other):
+        self.check_dimension(other)
+
         result = []
 
         for i in range(len(self.values)):
@@ -17,6 +29,8 @@ class Vector:
         return Vector(result)
 
     def __sub__(self, other):
+        self.check_dimension(other)
+
         result = []
 
         for i in range(len(self.values)):
@@ -32,7 +46,12 @@ class Vector:
 
         return Vector(result)
 
+    def __rmul__(self, scalar):
+        return self * scalar
+
     def dot(self, other):
+        self.check_dimension(other)
+
         result = 0
 
         for i in range(len(self.values)):
@@ -49,18 +68,39 @@ class Vector:
         return math.sqrt(total)
 
     def cosine_similarity(self, other):
+        self.check_dimension(other)
+
+        if self.norm() == 0 or other.norm() == 0:
+            raise ValueError("Cannot compute cosine similarity with zero vector")
+
         return self.dot(other) / (self.norm() * other.norm())
 
 
 v1 = Vector([2, 3])
 v2 = Vector([-1, 4])
+v3 = Vector([2, 3])
 
 print("v1:", v1)
 print("v2:", v2)
+
 print("v1 + v2:", v1 + v2)
 print("v1 - v2:", v1 - v2)
+
 print("v1 * 3:", v1 * 3)
+print("3 * v1:", 3 * v1)
+
 print("dot product:", v1.dot(v2))
 print("norm of v1:", v1.norm())
 print("norm of v2:", v2.norm())
+
 print("cosine similarity:", v1.cosine_similarity(v2))
+
+print("v1 == v2:", v1 == v2)
+print("v1 == v3:", v1 == v3)
+
+try:
+    v4 = Vector([1, 2, 3])
+    print(v1 + v4)
+except ValueError as error:
+    print(error)
+
